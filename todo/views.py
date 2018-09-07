93507ce09e4358189224c9b2419b295fb1f986d8
profran <francescosilvetti8@gmail.com>
2018-09-07 10:38:47 -0300

    Added views for Create, archive and delete ToDo

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from todo.models import ToDo
 
 # Create your views here.
+
+def createToDo(request):
+
+    if (request.POST):
+        user = request.user
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        archived = request.POST.get('archived')
+        todo = ToDo(user=user, title=title, description=description, archived=archived)
+        todo.save()
+
+def archiveToDo(request):
+
+    if (request.POST):
+
+        todo = ToDo.objects.get(id=request.POST.get('id'))
+
+        if (request.user == todo.user):
+            todo.archived = True
+
+def deleteToDo(request):
+    if (request.POST):
+
+        todo = ToDo.objects.get(id=request.POST.get('id'))
+
+        if (request.user == todo.user):
+            todo.delete()
