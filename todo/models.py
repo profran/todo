9da2ccbbcc3dac6d0be768e4e87a311060af3ee0
profran <francescosilvetti8@gmail.com>
2018-09-07 10:26:21 -0300

    Added ToDo Model, registered in the admin

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class ToDo(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.TextField(max_length=20, null=False)
+    description = models.TextField(max_length=144, null=True)
+    archived = models.BooleanField(default=False, null=False)
+
